4501d67cdef4fdc6f00f20c4f4eb3947f5e7999d
kawsar32111 <140530395+kawsar32111@users.noreply.github.com>
2024-01-07 21:44:54 +0600

    added html css files for the cv

---
+++ hii.js
@@ -0,0 +1,119 @@
+// string method
+
+
+// 1= toUpperCase
+
+// let num = " i love you";
+// let sum = num.toUpperCase();
+// console.log(sum);
+
+// 2== toLowerCase
+// let num = "I LOVE YOU";
+// let sum =num.toLowerCase();
+// console.log(sum);
+
+//3== split
+
+// let num = "'kawsar' 'ali' 'hablu vaiya' 'nahid'";
+// let sum = num.split('kawsar');
+// console.log(sum);
+
+
+//4== indexOf
+//  let num = "i love you";
+//  let sum = num.indexOf("y");
+//  console.log(sum);
+
+//5== length
+
+// let num = "i love you";
+// let sum = num.length;
+// console.log(sum);
+
+
+//6 slice
+
+// let num = "i love you";
+// let sum = num.slice(2,7);
+// console.log(sum);
+
+
+//7== replase
+// let num = "i love you";
+// let sum = num.replace("you", "ma");
+// console.log(sum);
+
+//8==concat
+// let num = "i love";
+// let num1 = "you";
+// let sum = num.concat(" ", num1);
+// console.log(sum);
+
+//9== charAt
+
+// let num = "i love you";
+// let sum = num.charAt(3);
+// console.log(sum);
+
+//10== charCodeAt
+// let num = "i love you";
+// let sum = num.charCodeAt(3);
+// console.log(sum);
+
+//11== codePointAt
+// let num ="i love you";
+// let sum = num.codePointAt(3);
+// console.log(sum);
+
+//12== endwidth
+// let num = "i love you";
+// let sum =num.startsWith("i");
+// console.log(sum);
+
+
+//13== startswidth
+// let num = "i love you";
+// let sum = num.endsWith ("you");
+// console.log(sum);
+
+//14== includes
+
+// let num = "love";
+// let sum = num.includes("love");
+// console.log(sum);
+
+//15== trimStart
+
+// let num = "    i love you    ";
+// let sum = num.trimStart();
+// console.log(sum);
+
+
+//16== trimEnd
+
+// let num = "   i love you   ";
+// let sum = num.trimEnd();
+// console.log(sum);
+
+//17== trim
+
+// let num = "i love you";
+// let sum = num.trim();
+// console.log(sum);
+
+//18== padEnd
+
+// let num = "i love ";
+// let sum = num.padEnd(10,"you");
+// console.log(sum);
+
+//19== padStart
+
+// let num = "i love you";
+// let sum = num.padEnd(14,"baba");
+// console.log(sum);
+
+//20==repeat
+// let num = "i love you";
+// let sum = num.repeat(2);
+// console.log(sum);
